b4551ae5a5f1093e31497750782999526144fdfd
VanessaCalimag <vanessa.calimag@my.jcu.edu.au>
2023-09-02 22:35:52 +1000

    Testing cfooter changes

--- twentytwentytwo-child/footer-default.php
+++ twentytwentytwo-child/footer-default.php
@@ -14,7 +14,7 @@ return array(
 					<p class="has-text-align-right">' .
 					sprintf(
 						/* Translators: WordPress link. */
-						esc_html__( 'Proud powered by %s', 'twentytwentytwo' ),
+						esc_html__( 'Copyright powered by %s', 'twentytwentytwo' ),
 						'<a href="' . esc_url( __( 'https://wordpress.org', 'twentytwentytwo' ) ) . '" rel="nofollow">WordPress</a>'
 					) . '</p>
 					<!-- /wp:paragraph --></div>
