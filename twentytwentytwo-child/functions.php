c02a30eb625c770da7a8f8369f7f131179a368e7
VanessaCalimag <vanessa.calimag@my.jcu.edu.au>
2023-09-15 07:25:54 +1000

    Updated code to load the website

--- twentytwentytwo-child/functions.php
+++ twentytwentytwo-child/functions.php
@@ -1,6 +1,6 @@
 <?php
-add_action( 'wp_enqueue_scripts', 'twentytwentytwo_child_styles' );
-function twentytwentytwo_child_styles() {
-    wp_enqueue_style( 'child-style', get_stylesheet_uri(),
-    array( 'twentytwentytwo-style' ), wp_get_theme()->get( 'Version' ) );
+add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
+function enqueue_parent_styles() {
+wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
+?>
