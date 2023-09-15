9ebc01184b30e02beacc66811876e42c16f878f9
VanessaCalimag <vanessa.calimag@my.jcu.edu.au>
2023-09-15 20:27:12 +1000

    Updated styling of forms

--- twentytwentytwo-child/functions.php
+++ twentytwentytwo-child/functions.php
@@ -1,6 +1,7 @@
 <?php
-add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
-function enqueue_parent_styles() {
-wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+add_action( 'wp_enqueue_scripts', 'twentytwentytwo_child_styles' );
+function twentytwentytwo_child_styles() {
+    wp_enqueue_style( 'child-style', get_stylesheet_uri(),
+    array( 'twentytwentytwo-style' ), wp_get_theme()->get( 'Version' ) );
 }
 ?>
